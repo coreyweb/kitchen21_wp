3be59ddec14f6c69ef8e96e27ff49a8a01eed84d
Corey Brown <coreyweb@gmail.com>
2017-05-08 22:14:29 -0400

    Clean up About Us template layout and structure

--- layouts/page-about.php
+++ layouts/page-about.php
@@ -9,24 +9,13 @@
 
 get_header(); ?>
 
-    <article class="page">
-      <div class="shelf-header ">
-        <h1>
-          <?php the_title() ?>
-        </h1>
-        <img src="<?php vt_resize(get_the_ID(), 1200, 284, true) ?>" alt="About Kitchen 21 image" class="img-responsive hidden-xs">
-        <img src="<?php vt_resize(get_the_ID(), 767, 587, true) ?>" alt="About Kitchen 21 image" class="img-responsive visible-xs">
-      </div><!-- .shelf-header -->
+<?php
+  while ( have_posts() ) : the_post();
 
-      <?php
-        while ( have_posts() ) : the_post();
+    get_template_part( 'template-parts/content', 'page' );
 
-          get_template_part( 'template-parts/content', 'page' );
-
-        endwhile; // End of the loop.
-      ?>
-      
-    </article><!-- .page -->
+  endwhile; // End of the loop.
+?>
       
     <div class="block">
       <div class="grid">
--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -8,13 +8,23 @@
  */
 
 ?>
+    <article class="page">
+      <div class="shelf-header ">
+        <h1>
+          <?php the_title() ?>
+        </h1>
+        <img src="<?php vt_resize(get_the_ID(), 1200, 284, true) ?>" alt="<?php the_title() ?> image" class="img-responsive hidden-xs">
+        <img src="<?php vt_resize(get_the_ID(), 767, 587, true) ?>" alt="<?php the_title() ?> image" class="img-responsive visible-xs">
+      </div><!-- .shelf-header -->
 
-  <?php
-    the_content();
-  ?>
+      <?php
+        the_content();
+      ?>
 
-  <?php
-    if ( current_user_can('edit_post') ) :
-      echo '<p class="text-center"><a href="' . get_edit_post_link() . '" class="btn btn-info">Edit Page</a>';
-    endif;
-  ?>
+      <?php
+        if ( current_user_can('edit_post') ) :
+          echo '<p class="text-center"><a href="' . get_edit_post_link() . '" class="btn btn-info">Edit Page</a>';
+        endif;
+      ?>
+
+    </article><!-- .page -->
