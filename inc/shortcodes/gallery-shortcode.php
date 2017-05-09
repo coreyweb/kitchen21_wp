32bd86feb95e2f0b27a4baeda7a2682b08dfcbb7
Corey Brown <coreyweb@gmail.com>
2017-05-08 22:43:57 -0400

    Update Gallery shortcode to K21 markup

--- inc/shortcodes/gallery-shortcode.php
+++ inc/shortcodes/gallery-shortcode.php
@@ -1,228 +1,101 @@
-<?php
-remove_shortcode( 'gallery' );
-add_shortcode( 'gallery', 'Kitchen21_gallery_shortcode' );
+<?php 
+// Remove built in shortcode
+remove_shortcode('gallery', 'gallery_shortcode');
+add_shortcode('gallery', 'carousel_shortcode');
 
-/**
- * Builds the Gallery shortcode output.
- *
- * This implements the functionality of the Gallery Shortcode for displaying
- * WordPress images on a post.
- *
- * @since 2.5.0
- *
- * @param array $attr {
- *     Attributes of the gallery shortcode.
- *
- * @type string $order Order of the images in the gallery. Default 'ASC'. Accepts 'ASC', 'DESC'.
- * @type string $orderby The field to use when ordering the images. Default 'menu_order ID'.
- *                              Accepts any valid SQL ORDERBY statement.
- * @type int $id Post ID.
- * @type string $itemtag HTML tag to use for each image in the gallery.
- *                              Default 'dl', or 'figure' when the theme registers HTML5 gallery support.
- * @type string $icontag HTML tag to use for each image's icon.
- *                              Default 'dt', or 'div' when the theme registers HTML5 gallery support.
- * @type string $captiontag HTML tag to use for each image's caption.
- *                              Default 'dd', or 'figcaption' when the theme registers HTML5 gallery support.
- * @type int $columns Number of columns of images to display. Default 3.
- * @type string $size Size of the images to display. Default 'thumbnail'.
- * @type string $ids A comma-separated list of IDs of attachments to display. Default empty.
- * @type string $include A comma-separated list of IDs of attachments to include. Default empty.
- * @type string $exclude A comma-separated list of IDs of attachments to exclude. Default empty.
- * @type string $link What to link each image to. Default empty (links to the attachment page).
- *                              Accepts 'file', 'none'.
- * }
- * @return string HTML content to display gallery.
- */
-function Kitchen21_gallery_shortcode( $attr ) {
-	$post = get_post();
+// Replace with custom shortcode
+function carousel_shortcode($attr) {
+    $post = get_post();
 
-	static $instance = 0;
-	$instance ++;
+    static $instance = 0;
+    $instance++;
 
-	if ( ! empty( $attr['ids'] ) ) {
-// 'ids' is explicitly ordered, unless you specify otherwise.
-		if ( empty( $attr['orderby'] ) ) {
-			$attr['orderby'] = 'post__in';
-		}
-		$attr['include'] = $attr['ids'];
-	}
+    if (!empty($attr['ids'])) {
+        if (empty($attr['orderby'])) {
+            $attr['orderby'] = 'post__in';
+        }
+        $attr['include'] = $attr['ids'];
+    }
 
-	/**
-	 * Filter the default gallery shortcode output.
-	 *
-	 * If the filtered output isn't empty, it will be used instead of generating
-	 * the default gallery template.
-	 *
-	 * @since 2.5.0
-	 * @since 4.2.0 The `$instance` parameter was added.
-	 *
-	 * @see gallery_shortcode()
-	 *
-	 * @param string $output The gallery output. Default empty.
-	 * @param array $attr Attributes of the gallery shortcode.
-	 * @param int $instance Unique numeric ID of this gallery shortcode instance.
-	 */
-	$output = apply_filters( 'post_gallery', '', $attr, $instance );
-	if ( $output != '' ) {
-		return $output;
-	}
+    $output = apply_filters('post_gallery', '', $attr);
 
-	$html5 = current_theme_supports( 'html5', 'gallery' );
-	$atts  = shortcode_atts( array(
-		'order'      => 'ASC',
-		'orderby'    => 'menu_order ID',
-		'id'         => $post ? $post->ID : 0,
-		'itemtag'    => $html5 ? 'figure' : 'dl',
-		'icontag'    => $html5 ? 'div' : 'dt',
-		'captiontag' => $html5 ? 'figcaption' : 'dd',
-		'columns'    => 3,
-		'size'       => 'thumbnail',
-		'include'    => '',
-		'exclude'    => '',
-		'link'       => ''
-	), $attr, 'gallery' );
+    if ($output != '') {
+        return $output;
+    }
 
-	$id = intval( $atts['id'] );
+    if (isset($attr['orderby'])) {
+        $attr['orderby'] = sanitize_sql_orderby($attr['orderby']);
+        if (!$attr['orderby']) {
+            unset($attr['orderby']);
+        }
+    }
 
-	if ( ! empty( $atts['include'] ) ) {
-		$_attachments = get_posts( array( 'include'        => $atts['include'],
-		                                  'post_status'    => 'inherit',
-		                                  'post_type'      => 'attachment',
-		                                  'post_mime_type' => 'image',
-		                                  'order'          => $atts['order'],
-		                                  'orderby'        => $atts['orderby']
-		) );
+    extract(shortcode_atts(array(
+        'order' => 'ASC',
+        'orderby' => 'menu_order ID',
+        'id' => $post->ID,
+        'itemtag' => '',
+        'icontag' => '',
+        'captiontag' => '',
+        'columns' => 3,
+        'size' => 'thumbnail',
+        'include' => '',
+        'link' => '',
+        'exclude' => ''
+                    ), $attr));
 
-		$attachments = array();
-		foreach ( $_attachments as $key => $val ) {
-			$attachments[ $val->ID ] = $_attachments[ $key ];
-		}
-	} elseif ( ! empty( $atts['exclude'] ) ) {
-		$attachments = get_children( array( 'post_parent'    => $id,
-		                                    'exclude'        => $atts['exclude'],
-		                                    'post_status'    => 'inherit',
-		                                    'post_type'      => 'attachment',
-		                                    'post_mime_type' => 'image',
-		                                    'order'          => $atts['order'],
-		                                    'orderby'        => $atts['orderby']
-		) );
-	} else {
-		$attachments = get_children( array( 'post_parent'    => $id,
-		                                    'post_status'    => 'inherit',
-		                                    'post_type'      => 'attachment',
-		                                    'post_mime_type' => 'image',
-		                                    'order'          => $atts['order'],
-		                                    'orderby'        => $atts['orderby']
-		) );
-	}
+    $id = intval($id);
 
-	if ( empty( $attachments ) ) {
-		return '';
-	}
+    if ($order === 'RAND') {
+        $orderby = 'none';
+    }
 
-	if ( is_feed() ) {
-		$output = "\n";
-		foreach ( $attachments as $att_id => $attachment ) {
-			$output .= wp_get_attachment_link( $att_id, $atts['size'], true ) . "\n";
-		}
+    if (!empty($include)) {
+        $_attachments = get_posts(array('include' => $include, 'post_status' => 'inherit', 'post_type' => 'attachment', 'post_mime_type' => 'image', 'order' => $order, 'orderby' => $orderby));
 
-		return $output;
-	}
+        $attachments = array();
+        foreach ($_attachments as $key => $val) {
+            $attachments[$val->ID] = $_attachments[$key];
+        }
+    } elseif (!empty($exclude)) {
+        $attachments = get_children(array('post_parent' => $id, 'exclude' => $exclude, 'post_status' => 'inherit', 'post_type' => 'attachment', 'post_mime_type' => 'image', 'order' => $order, 'orderby' => $orderby));
+    } else {
+        $attachments = get_children(array('post_parent' => $id, 'post_status' => 'inherit', 'post_type' => 'attachment', 'post_mime_type' => 'image', 'order' => $order, 'orderby' => $orderby));
+    }
 
-	$itemtag    = tag_escape( $atts['itemtag'] );
-	$captiontag = tag_escape( $atts['captiontag'] );
-	$icontag    = tag_escape( $atts['icontag'] );
-	$valid_tags = wp_kses_allowed_html( 'post' );
-	if ( ! isset( $valid_tags[ $itemtag ] ) ) {
-		$itemtag = 'dl';
-	}
-	if ( ! isset( $valid_tags[ $captiontag ] ) ) {
-		$captiontag = 'dd';
-	}
-	if ( ! isset( $valid_tags[ $icontag ] ) ) {
-		$icontag = 'dt';
-	}
+    if (empty($attachments)) {
+        return '';
+    }
 
-	$columns   = intval( $atts['columns'] );
-	$itemwidth = $columns > 0 ? floor( 100 / $columns ) : 100;
-	$float     = is_rtl() ? 'right' : 'left';
+    //Output Begins Here
+    $output = '<div id="carousel-gallery" class="carousel slide" data-ride="carousel">';
+    $output .= '  <ol class="carousel-indicators">';
+    $indicator = 0;
+    foreach ($attachments as $id => $attachment) {
+      if ( $indicator == '0' ) { $active_indicator = ' class="active"'; } else { $active_indicator = ''; }      
+      $output .= '    <li data-target="#carousel-gallery" data-slide-to="' . $indicator . '"' . $active_indicator . '></li>';
+      $indicator++;      
+    }
+    $output .= '  </ol>';
+    $output .= '  <div class="carousel-inner" role="listbox">'; 
+		$size = 'full';
 
-	$selector = "gallery-{$instance}";
+    //Begin counting slides to set the first one as the active class
+    $counter = 0;
+    foreach ($attachments as $id => $attachment) {
+  		$image_alt = get_post_meta( $id, '_wp_attachment_image_alt', true);
+      $link = isset($attr['link']) && 'file' == $attr['link'] ? wp_get_attachment_link($id, $size, false, false) : wp_get_attachment_link($id, $size, true, false);
 
-	$gallery_style = '';
+      $image_src_url = wp_get_attachment_image_src($id, $size);
+      if ( $counter == '0' ) { $active = ' active'; } else { $active = ''; }      
+      $output .= '<div class="item' . $active . '">';
+      $output .= '  <img src="' . $image_src_url[0] . '" alt="'. $image_alt . '">';
+      $output .= '</div><!-- .item -->';
+      $counter++;
+    }
 
-	/**
-	 * Filter whether to print default gallery styles.
-	 *
-	 * @since 3.1.0
-	 *
-	 * @param bool $print Whether to print default gallery styles.
-	 *                    Defaults to false if the theme supports HTML5 galleries.
-	 *                    Otherwise, defaults to true.
-	 */
-	if ( apply_filters( 'use_default_gallery_style', ! $html5 ) ) {
-		$gallery_style = "
-			<style type='text/css'>
-				#{$selector} {
-					margin: auto;
-				}
-				#{$selector} .gallery-item {
-				float: {$float};
-				margin-top: 10px;
-				text-align: center;
-				width: {$itemwidth}%;
-				}
-				#{$selector} img {
-					border: 2px solid #cfcfcf;
-				}
-				#{$selector} .gallery-caption {
-					margin-left: 0;
-				}
-				/* see gallery_shortcode() in wp-includes/media.php */
-			</style>\n\t\t";
-	}
+    $output .= '        </div><!-- .carousel-inner -->';
+    $output .= '      </div><!-- .carousel -->';
 
-	$size_class  = sanitize_html_class( $atts['size'] );
-	$gallery_div = "<div id='$selector' class='gallery galleryid-{$id} gallery-columns-{$columns} gallery-size-{$size_class}'>";
-
-	/**
-	 * Filter the default gallery shortcode CSS styles.
-	 *
-	 * @since 2.5.0
-	 *
-	 * @param string $gallery_style Default CSS styles and opening HTML div container
-	 *                              for the gallery shortcode output.
-	 */
-
-
-
-	$i = 0;
-	$slides = '';
-	$nav = '';
-	foreach ( $attachments as $id => $attachment ) {
-
-		$attr = ( trim( $attachment->post_excerpt ) ) ? array( 'aria-describedby' => "$selector-$id" ) : '';
-
-		$orientation = '';
-		ob_start();
-		?>
-		    <div><img src="<?php echo esc_url( get_vt_resize( $id, 840, 560, true )['url'] ) ?>" class="img-responsive" alt="<?php echo esc_attr($attr) ?>"></div>
-		<?php
-		$slides .= ob_get_clean();
-		ob_start(); ?>
-		    <div><img src="<?php echo esc_url( get_vt_resize( $id, 200, 200, true )['url'] ) ?>" class="img-responsive" alt=""></div>
-		<?php
-		$nav .=  ob_get_clean();
-	}
-
-	$output = '<div class="module photo-carousel-wrap">';
-	$output .= '<div class="slider slider-for">';
-	$output .= $slides;
-	$output .= '</div>'; //close slider-for
-	$output .= '<div class="slider slider-nav">';
-	$output .= $nav;
-	$output .= '</div>'; //close slider-nav
-	$output .= '</div><!-- .module photo-carousel-wrap -->';
-
-	return $output;
+    return $output;
 }
