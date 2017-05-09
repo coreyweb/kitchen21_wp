07cb856e491a4d63fc229310d37cf8af2d649a53
Corey Brown <coreyweb@gmail.com>
2017-05-09 08:37:16 -0400

    First attempt at fixing active state for main navigation

--- functions.php
+++ functions.php
@@ -74,7 +74,7 @@ add_action( 'after_setup_theme', 'Kitchen21_setup' );
   add_filter( 'show_admin_bar', '__return_false' );
 
 // Include Mailchimp API functionality
-  require_once('lib/mailchimp/mailchimp.php' );
+  // require_once('lib/mailchimp/mailchimp.php' );
   
 // Scripts and style rules
 function load_styles() {
@@ -127,3 +127,22 @@ add_filter( 'wp_editor_set_quality', 'my_image_quality' );
 
 // remove auto paragraphs from category descriptions
 remove_filter('term_description','wpautop');
+
+
+// fix parent rules for active nav state on custom post types
+add_filter('nav_menu_css_class', 'current_type_nav_class', 10, 2);
+function current_type_nav_class($classes, $item) {
+    // Get post_type for this post
+    $post_type = get_query_var('post_type');
+    // Removes current_page_parent class from blog menu item
+    if ( get_post_type() == $post_type )
+        $classes = array_filter($classes, "get_current_value" );
+    // Go to Menus and add a menu class named: {custom-post-type}-menu-item
+    // This adds a current_page_parent class to the parent menu item
+    if( in_array( $post_type.'-menu-item', $classes ) )
+      array_push($classes, 'current_page_parent');
+    return $classes;
+}
+function get_current_value( $element ) {
+    return ( $element != "current_page_parent" );
+}
