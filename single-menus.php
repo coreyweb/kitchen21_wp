fb20e161e47e34a866a88f1d743dba80956f4f48
Corey Brown <coreyweb@gmail.com>
2017-05-11 16:45:02 -0400

    Updated Menu detail page to new template structure

--- single-menus.php
+++ single-menus.php
@@ -11,9 +11,44 @@ get_header(); ?>
   
 	<?php
   	while ( have_posts() ) : the_post();
+  ?>
 
-  	  get_template_part( 'template-parts/content', 'single-menus' );
+    <div class="block">
+      <div class="row">
+        <article class="menu-wrap">
+          <div class="shelf-header header-menu">
+            <h1><?php the_title() ?></h1>
 
+            <?php if ( has_post_thumbnail() ) : ?>
+
+            <img src="<?php vt_resize(get_the_ID(), 1000, 300, true) ?>" alt="<?php the_title() ?> image" class="img-responsive hidden-xs">
+            <img src="<?php vt_resize(get_the_ID(), 767, 767, true) ?>" alt="<?php the_title() ?> image" class="img-responsive visible-xs">
+
+            <?php endif; ?>
+
+          </div><!-- .shelf-header -->
+
+          <?php the_content() ?>        
+
+          <div class="menu">
+            <?php 
+              if ( get_post_meta($post->ID, 'menu_pdf', true) ) :
+                echo get_post_meta($post->ID, 'menu_pdf', true);
+              endif;
+            ?>
+            
+            <?php
+              if ( current_user_can('edit_post') ) :
+                echo '<p class="text-center"><a href="' . get_edit_post_link() . '" class="btn btn-info">Edit Page</a>';
+              endif;
+            ?>
+            
+          </div><!-- .menu -->
+        </article><!-- .menu-wrap -->
+      </div><!-- .row -->
+    </div><!-- .block -->
+
+  <?php
   	endwhile; // End of the loop.
 	?>
 
