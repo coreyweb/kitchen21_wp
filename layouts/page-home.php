3c75441938e9914779c7c49abaf6cf37b1e4a16e
Corey Brown <coreyweb@gmail.com>
2017-05-10 20:08:29 -0400

    Fix address on map

--- layouts/page-home.php
+++ layouts/page-home.php
@@ -36,14 +36,14 @@ get_header(); ?>
           <div class="row vertical-align">
             <div class="col-sm-6">
               <div class="embed-responsive embed-responsive-16by9">
-                <iframe class="embed-responsive-item" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3030.550352110494!2d-73.98963468488753!3d40.573605879347205!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c245cbeab87bd7%3A0x2eb9a72b4b093331!2s3051+W+21st+St%2C+Brooklyn%2C+NY+11224!5e0!3m2!1sen!2sus!4v1493843831832" width="500" height="400"></iframe>
+                <iframe class="embed-responsive-item" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3030.550352110494!2d-73.98963468488753!3d40.573605879347205!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c245cbeab87bd7%3A0x2eb9a72b4b093331!2s3052+W+21st+St%2C+Brooklyn%2C+NY+11224!5e0!3m2!1sen!2sus!4v1493843831832" width="500" height="400"></iframe>
               </div>
             </div><!-- .col-sm-6 -->
             <div class="col-sm-6">
               <div class="has-arrow-left">
                 <h3 class="highlight">Stop by and grab a bite!</h3>
                 <p>
-                  3051 W. 21st Street Brooklyn, NY 11224<br>
+                  3052 W. 21st Street Brooklyn, NY 11224<br>
                   Hours: Monday – Sunday: 11 AM – 11 PM<br>
                   <a href="tel:718-954-9801">718-954-9801</a>
                 </p>
