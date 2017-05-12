216cc4c59cc31d95ff06b2897f18ca3585fa81d9
Corey Brown <coreyweb@gmail.com>
2017-05-12 12:22:12 -0400

    Refactored home page (added in managed content for all contact info, physical address, etc.)

--- layouts/page-home.php
+++ layouts/page-home.php
@@ -34,24 +34,69 @@ get_header(); ?>
         </div><!-- .block -->
         <div class="block">
           <div class="row vertical-align">
+
             <div class="col-sm-6">
+              <?php 
+                query_posts(
+                  array(
+                    'post_type' => 'contact_info',
+                    'showposts' => 1,
+                    'post_status' => 'publish'
+                  )
+                );
+
+                if ( have_posts() ) : 
+                  while ( have_posts() ) : the_post();
+                
+                  $address = get_post_meta($post->ID, 'restaurant_address', true);
+
+                  endwhile;
+                endif; 
+            ?>          
+              
               <div class="embed-responsive embed-responsive-16by9">
-                <iframe class="embed-responsive-item" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3030.550352110494!2d-73.98963468488753!3d40.573605879347205!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c245cbeab87bd7%3A0x2eb9a72b4b093331!2s3052+W+21st+St%2C+Brooklyn%2C+NY+11224!5e0!3m2!1sen!2sus!4v1493843831832" width="500" height="400"></iframe>
+                <iframe class="embed-responsive-item" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3030.550352110494!2d-73.98963468488753!3d40.573605879347205!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c245cbeab87bd7%3A0x2eb9a72b4b093331!2s<?php echo urlencode($address); ?>!5e0!3m2!1sen!2sus!4v1493843831832" width="500" height="400"></iframe>
               </div>
             </div><!-- .col-sm-6 -->
+            
             <div class="col-sm-6">
               <div class="has-arrow-left">
                 <h3 class="highlight">Stop by and grab a bite!</h3>
+
+                <?php 
+                  query_posts(
+                    array(
+                      'post_type' => 'contact_info',
+                      'showposts' => 1,
+                      'post_status' => 'publish'
+                    )
+                  );
+
+                  if ( have_posts() ) : while ( have_posts() ) : the_post();
+                ?>
+
                 <p>
-                  3052 W. 21st Street Brooklyn, NY 11224<br>
-                  Hours: Monday – Sunday: 11 AM – 11 PM<br>
-                  <a href="tel:718-954-9801">718-954-9801</a>
+                  <?php if ( get_post_meta($post->ID, 'restaurant_address', true) ) : echo get_post_meta($post->ID, 'restaurant_address', true) ?><br>
+                  <?php endif; ?>
+
+                  <?php if ( get_post_meta($post->ID, 'hours', true) ) : ?>
+                  Hours: <?php echo get_post_meta($post->ID, 'hours', true) ?><br>
+                  <?php endif; ?>
+
+                  <?php if ( get_post_meta($post->ID, 'phone_number', true) ) : ?>
+                  <a href="tel:<?php echo get_post_meta($post->ID, 'phone_number', true) ?>"><?php echo get_post_meta($post->ID, 'phone_number', true) ?></a>
+                  <?php endif; ?>                  
                 </p>
+                
+                <?php 
+                  endwhile;
+                endif; 
+                ?>
+
               </div><!-- .has-arrow-left -->
             </div><!-- .col-sm-6 -->
           </div><!-- .row -->
         </div><!-- .block -->
 
 <?php
-// get_sidebar();
 get_footer();
